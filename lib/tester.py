455d1bec82e15172babfad3a158b68dda1dbad60
pmsinner <peter.apsbox@yandex.ru>
2018-07-13 14:40:58 +0300

    add: img-based sample generation

--- lib/tester.py
+++ lib/tester.py
@@ -16,6 +16,24 @@ def sample_batch(n, size=5):
     q = np.random.dirichlet(np.ones(5), size=1).ravel()
     return C, p, q
 
+def sample_img_batch(n, max_int=10):
+    """Sample image-based batch for trasport problem 
+    
+    Arguments:
+        n {int} -- NxX image size 
+    
+    Keyword Arguments:
+        max_int {int} -- maximum color saturation (default: {10})
+    """
+    img1, img2 = np.random.randint(1, high=(max_int+1), size=(2, n, n))
+    C = np.zeros((n ** 2, n ** 2))
+    for i in range(n ** 2):
+        for j in range(n ** 2):
+            C[i, j] = norm(np.array([i // n, i % n]) - np.array([j // n, j % n]), 2)
+    p = img1.reshape((n ** 2, )) / np.sum(img1)
+    q = img2.reshape((n ** 2, )) / np.sum(img2)
+    return C, p, q
+
 def convergence_test(method, gammas, n=5, n_exp=5):
     """Convergence tests for regularized optimization methods
     
