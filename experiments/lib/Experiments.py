862daa3c76af247615cae04f5f029b19d8c62615
Dmitry Pasechnyuk <pasechnyuk2004@gmail.com>
2018-07-17 14:29:34 +0300

    .

--- experiments/lib/Experiments.py
+++ experiments/lib/Experiments.py
@@ -6,8 +6,7 @@ from tqdm import tqdm
 
 class Experiments:
     @staticmethod
-    def load_image(filename):
-        basewidth = 10
+    def load_image(filename, basewidth):
         img = Image.open(filename)
         wpercent = (basewidth / float(img.size[0]))
         hsize = int((float(img.size[1]) * float(wpercent)))
@@ -15,9 +14,9 @@ class Experiments:
         return np.asarray(img, dtype="int32")
 
     @staticmethod
-    def load_data(img1_path='1.png', img2_path='2.png'):
-        img1 = Experiments.load_image(img1_path)
-        img2 = Experiments.load_image(img2_path)
+    def load_data(img1_path='1.png', img2_path='2.png', show=False, size=10):
+        img1 = Experiments.load_image(img1_path, size)
+        img2 = Experiments.load_image(img2_path, size)
 
         C = np.zeros((img1.shape[0] * img1.shape[1], img1.shape[0] * img1.shape[1]))
 
@@ -31,12 +30,13 @@ class Experiments:
         p = img1.reshape((img1.shape[0] * img1.shape[1], )) / np.sum(img1)
         q = img2.reshape((img2.shape[0] * img2.shape[1], )) / np.sum(img2)
 
-        plt.figure()
-        plt.subplot(121)
-        plt.imshow(img1, cmap='gray')
-        plt.subplot(122)
-        plt.imshow(img2, cmap='gray')
-        plt.show()
+        if show:
+            plt.figure()
+            plt.subplot(121)
+            plt.imshow(img1, cmap='gray')
+            plt.subplot(122)
+            plt.imshow(img2, cmap='gray')
+            plt.show()
         
         return C, p, q
     
@@ -45,24 +45,25 @@ class Experiments:
         plt.imshow(x.reshape(int(math.sqrt(n)), int(math.sqrt(n)), n).sum(2))
     
     @staticmethod
-    def test(C, p, q, config={'eps': (0.01, 0.01, 1), 'gamma': (10, 0.01, 2)}, methods=[]):
+    def test(problems, config={'eps': (0.01, 0.01, 1), 'gamma': (10, 0.01, 2)}, methods=[]):
         epsilons = [config['eps'][0] / config['eps'][2]**i for i in range(int(math.log(config['eps'][0] / config['eps'][1], config['eps'][2] + 1e-5)) + 1)]
         gammas = [config['gamma'][0] / config['gamma'][2]**i for i in range(int(math.log(config['gamma'][0] / config['gamma'][1], config['gamma'][2] + 1e-5)) + 1)]
 
         iterations = [[] for i in range(len(methods))]
 
-        with tqdm(total=len(epsilons) * len(gammas) * len(methods)) as ph:
-            for eps in epsilons:
-                for gamma in gammas:
-                    for i in range(len(methods)):
-                        x, iterations_num, _ = methods[i](C, p, q, gamma, eps)
-                        iterations[i].append(iterations_num)
-                        ph.update(1)
+        with tqdm(total=len(epsilons) * len(gammas) * len(methods) * len(problems)) as ph:
+            for (C, p, q) in problems:
+                for eps in epsilons:
+                    for gamma in gammas:
+                        for i in range(len(methods)):
+                            x, iterations_num, _ = methods[i](C, p, q, gamma, eps)
+                            iterations[i].append(iterations_num)
+                            ph.update(1)
             
-        return epsilons, gammas, np.array(iterations).reshape((len(epsilons), len(methods), len(gammas)))
+        return epsilons, gammas, np.array(iterations).reshape((len(epsilons), len(methods), len(problems), len(gammas)))
     
     @staticmethod
-    def plot_algorithm_comparition(gammas, iterations, epsilon, n):
+    def plot_algorithm_comparation(gammas, iterations, epsilon, n):
         gamma_optimal = epsilon / (4 * np.log(n))
 
         gamma_plots_intersection = 0
