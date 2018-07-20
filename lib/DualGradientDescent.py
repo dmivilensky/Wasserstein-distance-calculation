3418d7b24390c15d49d477c95a2c409a26a01602
Dmitry Pasechnyuk <pasechnyuk2004@gmail.com>
2018-07-20 16:48:43 +0300

    code style

--- lib/DualGradientDescent.py
+++ lib/DualGradientDescent.py
@@ -13,34 +13,34 @@ class DualGradientDescent:
 
     def f(self, x):
         return (self.c * x).sum() + self.gamma * ((x + self.small) * np.log((x + self.small) / self.x_0)).sum()
-    
+
     def phi(self, lam, mu, n):
         return (lam * self.p).sum() + (mu * self.q).sum() + \
                 self.gamma * np.log(1/np.e * (self.x_0 * np.exp(
                     -(self.gamma + self.c + lam.repeat(n).reshape(-1, n) + mu.repeat(n).reshape(-1, n).T) / self.gamma
                 )).sum())
-        
+
     def x_hat(self, lam, mu, n):
         x_hat = self.x_0 * np.exp(
             -(self.gamma + self.c + lam.repeat(n).reshape(-1, n) + mu.repeat(n).reshape(-1, n).T) / self.gamma
         )
         return x_hat / x_hat.sum()
-    
+
     def _new_lm(self, p, q):
         x_hat = self.x_hat(self.lam, self.mu, self.n)
         return self.lam - self.gamma * (p - x_hat.sum(1)),\
                self.mu - self.gamma * (q - x_hat.sum(0))
-        
+
     def x_sum_update(self):
         self.x_sum += self.x_hat(self.lam, self.mu, self.n)
-        
+
     def _new_x_wave(self, k):
         return self.x_sum * 1/k
-    
+
     @staticmethod
     def deviation_p_q(x, p, q):
         return np.sqrt(np.sum((x.sum(1) - p)**2) + np.sum((x.sum(0) - q)**2))
-    
+
     def fit(self, c, p, q):
         self.c, self.p, self.q = c, p, q
         
