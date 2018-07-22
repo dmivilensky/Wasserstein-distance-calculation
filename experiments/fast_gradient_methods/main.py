0c15c4957c926dcc216e4a4c511c4bb1f6944e0b
Dikower <dmitriy1d01@gmail.com>
2018-07-22 18:38:22 +0300

    Update main.py

--- experiments/fast_gradient_methods/main.py
+++ experiments/fast_gradient_methods/main.py
@@ -6,7 +6,7 @@ import tqdm
 from multiprocessing.pool import ThreadPool
 
 
-def task_grandient(x):
+def task_gradient(x):
     return np.array([(i + 1) * 2 * x[i] for i in range(len(x))])
 
 
@@ -21,7 +21,7 @@ def nesterov_fastgrad(x, eps):
         k += 1
 
         argument = (x + ((k - 1)/(k + 2)) * (x - last_x))
-        grad = task_grandient(argument)
+        grad = task_gradient(argument)
         new_x = x - (1/L) * grad + ((k - 1)/(k + 2)) * (x - last_x)
 
         last_x = copy.copy(x)
@@ -40,7 +40,7 @@ def kim_fessler_gradient(x, eps):
     while True:
         k += 1
         last_t = t
-        grad = task_grandient(x)
+        grad = task_gradient(x)
         x_sum += last_t * grad
 
         teta = (1 + math.sqrt(8 * teta ** 2 + 1))/2
@@ -72,7 +72,7 @@ def nesterov_triangle_method(x, eps):
         A += a
 
         y = (a * u + last_A * x) / A
-        grad = task_grandient(y)
+        grad = task_gradient(y)
         u = u - a * grad
 
         x = (a * u + last_A * x) / A
@@ -81,36 +81,106 @@ def nesterov_triangle_method(x, eps):
             return x, k
 
 
-n = 30
+def find_r(y):
+    x = 0
+    j = 2
+    px = 0
+    while 1:
+        x = j
+        j *= 2
+        if task(y - px * task_gradient(y)) <= task(y - x * task_gradient(y)):
+            break
+        px = x
+    return x
+
+
+def ternary_search_beta(eps, l, r, u, x):
+    while(r - l > eps):
+        m1 = l + (r - l) / 3
+        m2 = r - (r - l) / 3
+        if(task(u + m1 * (x - u)) < task(u + m2 * (x - u))):
+            r = m2
+        else:
+            l = m1
+    return r
+
+
+def ternary_search_h(eps, l, r, y):
+    while(r - l > eps):
+        m1 = l + (r - l) / 3
+        m2 = r - (r - l) / 3
+        if(task(y - m1 * task_gradient(y)) < task(y - m2 * task_gradient(y))):
+            r = m2
+        else:
+            l = m1
+    return r
+
+
+def linear_coupling(x, eps):
+    alphprev = 0
+    xres = np.zeros(n)
+    xprev = x
+    uprev = xprev[:]
+    k = 0
+#        bcur = np.array([i / (i + 2) for i in range(n)])
+#        hcur = np.array([1 / L for i in range(n)])
+    Acur = 0
+    while(1):
+        bcur = ternary_search_beta(eps, 0, 1, uprev, xprev)
+        ycur = uprev + bcur * (xprev - uprev)
+        r = find_r(ycur)
+        hcur = ternary_search_h(eps, 0, r, ycur)
+        xcur = ycur - hcur * task_gradient(ycur)
+        Acur += alphprev
+        alphcur = ((task(ycur) - task(xcur)) + np.sqrt((task(ycur) - task(xcur)) * ((task(ycur) - task(xcur)) + 2 * Acur * np.linalg.norm(task_gradient(ycur)) ** 2)) )/ (np.linalg.norm(task_gradient(ycur)) ** 2)
+        ucur = uprev - alphcur * task_gradient(ycur)
+        # Here break criteria
+        if(task(xprev) - task(xres) <= eps):
+          break
+        xprev = xcur
+        uprev = ucur
+        alphprev = alphcur
+        k += 1
+
+    return xprev, k
+
+
+n = 10
 L = 2 * n
 x0 = np.array([i for i in range(n, 0, -1)], dtype="float64")
-results = [{}, {}, {}]
-dots = np.linspace(0.9, 0.00001, 1000)
-pool = ThreadPool(processes=3)
+
+results = [{}, {}, {}, {}]
+dots = np.linspace(0.01, 0.00000001, 1000)
+pool = ThreadPool(processes=len(results))
 
 for i in tqdm.tqdm(range(len(dots))):
     async_results = [pool.apply_async(nesterov_fastgrad, (x0, dots[i])),
                      pool.apply_async(kim_fessler_gradient, (x0, dots[i])),
-                     pool.apply_async(nesterov_triangle_method, (x0, dots[i]))]
-    for j in range(3):
+                     pool.apply_async(nesterov_triangle_method, (x0, dots[i])),
+                     pool.apply_async(linear_coupling, (x0, dots[i]))]
+    for j in range(len(async_results)):
         # results[j].append((math.log(1/dots[i]), math.log(async_results[j].get()[1])))
         # results[j].append((dots[i], async_results[j].get()[1]))
-        results[j][math.log(1/dots[i])] = math.log(async_results[j].get()[1])
+        if math.log(1/dots[i]) > 11:
+            results[j][math.log(1/dots[i])] = math.log(async_results[j].get()[1])
 
-print(results)
 
-
-plt.title('Сравнение эффективности методов минимизации')
+plt.title('Сравнение эффективности методов оптимизации')
 plt.ylabel('$\ln_{}(N) $')
-plt.xlabel('$\ln_{}(\\frac{1}{E})$')
+plt.xlabel('$\ln_{}(\\frac{1}{eps})$')
 plt.grid(color='b', linestyle=':', linewidth=0.5)
 # plt.xscale('log'), plt.xticks(fontsize=15)
 # plt.yscale('log'), plt.yticks(fontsize=15)
-
+x = [list(i.keys()) for i in results]
+y = [list(i.values()) for i in results]
+angles = [round((y[i][-1] - y[i][0])/(x[i][-1] - x[i][0]), 3) for i in range(len(results))]
 # plt.xticks(np.linspace(math.log(1/dots[0]), math.log(1/dots[-1]), 10))
-plt.plot(results[0].keys(), results[0].values(), color="blue", label="Nesterov 1983")
-plt.plot(results[1].keys(), results[1].values(), color="green", label="Kim-Fessler 2016")
-plt.plot(results[2].keys(), results[2].values(), color="orange", label="Nesterov 2016")
+
+plt.plot(x[0], y[0], color="#00693e", label="Nesterov 1983, k={}".format(angles[0]))
+plt.plot(x[1], y[1], color="#d2691e", label="Kim-Fessler 2016, k={}".format(angles[1]))
+plt.plot(x[2], y[2], color="#00677e", label="Nesterov 2016, k={}".format(angles[2]))
+plt.plot(x[3], y[3], color="#ed4830", label="LC with LS 2018, k={}".format(angles[3]))
+
 plt.legend()
 
 plt.show()
